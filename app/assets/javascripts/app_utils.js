43c4c731f0b2dfd42f3ac8d44f0dced0709e0d19
sanjiv <sanjiv@joshsoftware.com>
2013-04-24 18:26:26 +0530

    added idea and utils

--- app/assets/javascripts/app_utils.js
+++ app/assets/javascripts/app_utils.js
@@ -1,3 +1,13 @@
-var app = {}
+var app = {
+    
 
+}
+
+$(window).scroll(function(){
+  var yLeftToGo = document.height - (window.pageYOffset + window.innerHeight); 
+  if(yLeftToGo == 0){
+    //pagination logic need to be implemented
+    console.log('atbottom')
+  } 
+})
 
