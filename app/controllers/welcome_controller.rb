1b15a9b4b418fe4f382f39246d8a0aa64631b9ba
sanjiv <sanjiv@joshsoftware.com>
2013-04-27 00:53:27 +0530

    added handlebars template for displaying

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
+  include ActionController::Live
+  
   def index
     link_url = "http://www.therichest.org/entertainment/sexiest-chinese-actresses/"
     scraper = Webscraper::Multimedia.new(link_url)
     @contents = scraper.parse_chinese_actress()
+  
   end
 
 end
